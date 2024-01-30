539c8073afea94ab6828bb2681b594026342a30c
chajeongheon <jeongheon1127@gmail.com>
2024-01-30 14:14:33 +0900

    123

--- wedingrum/src/config/contansts.js
+++ wedingrum/src/config/contansts.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://www.kimjunnyoung.com/app3";
+export const API_URL = "http://3.131.43.198/app3";
 // export const API_URL = "https://lamp-shopping-server-7ingout.herokuapp.com";
